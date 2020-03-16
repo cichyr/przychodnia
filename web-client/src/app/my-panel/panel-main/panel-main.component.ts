7496b87a853d0b6637dbb48eddfb3582502256c0
rzepkamichal <michrzep@interia.pl>
2020-03-16 12:20:37 +0100

    Add unsubscribe example

--- web-client/src/app/my-panel/panel-main/panel-main.component.ts
+++ web-client/src/app/my-panel/panel-main/panel-main.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {HelloService} from '../../service/hello.service';
 import {HelloResponse} from '../../dto/hello-response';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-panel-main',
   templateUrl: './panel-main.component.html',
   styleUrls: ['./panel-main.component.css']
 })
-export class PanelMainComponent implements OnInit {
+export class PanelMainComponent implements OnInit, OnDestroy {
 
   someVariable = {
     "id": "3",
@@ -17,13 +18,20 @@ export class PanelMainComponent implements OnInit {
 
   helloResponse: HelloResponse;
   someArray = ['qwe', 'abc', 'xyz'];
+  subscription: Subscription;
 
   constructor(private helloService: HelloService) { }
 
   ngOnInit(): void {
-    this.helloService.getHelloMessage().subscribe(
+    this.subscription = this.helloService.getHelloMessage().subscribe(
       response => this.helloResponse = response
     );
+  }
+
+  //WAZNE!!!
+  //trzeba usunac suba bo inaczej bedzie wyciek pamieci
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
 }
