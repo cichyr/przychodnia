128916c2c795bfeb259be3036fe51dec328a8de8
Dominik Wolny <wolny.dominik@outlook.com>
2020-04-22 13:36:13 +0200

    Added ng-bootstrap to project

--- web-client/src/app/lab-exam/lab-exam-list/lab-exam-list.component.ts
+++ web-client/src/app/lab-exam/lab-exam-list/lab-exam-list.component.ts
@@ -6,15 +6,12 @@ import { LaboratoryExaminationService } from '../../service/laboratory-examinati
 
 @Component({
   selector: 'app-lab-exam-list',
-  templateUrl: './lab-exam-list.component.html',
-  styleUrls: ['./lab-exam-list.component.css']
+  templateUrl: './lab-exam-list.component.html'
 })
 export class LabExamListComponent implements OnInit {
 
   laboratoryList: LaboratoryExamination[]
   
-  someArray = ['qwe', 'abc', 'xyz']
-
   
 
   constructor(private laboratoryService: LaboratoryExaminationService) { }
--- web-client/src/app/lab-exam/lab-exam.module.ts
+++ web-client/src/app/lab-exam/lab-exam.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LabExamListComponent } from './lab-exam-list/lab-exam-list.component';
 
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap'
 
 
 @NgModule({
   declarations: [LabExamListComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    NgbModule
   ],
   exports: [
     LabExamListComponent
