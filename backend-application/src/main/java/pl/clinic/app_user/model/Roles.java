0a20592e3d3d240e7889569be3b86b2463ac6de7
rzepkamichal <michrzep@interia.pl>
2020-04-22 15:16:47 +0200

    Implement service association Account with User

---
+++ backend-application/src/main/java/pl/clinic/app_user/model/Roles.java
@@ -0,0 +1,10 @@
+package pl.clinic.app_user.model;
+
+public class Roles {
+
+    public static final String DOCTOR = "DOC";
+    public static final String RECEPTIONIST = "REC";
+    public static final String LAB_WORKER = "LABW";
+    public static final String LAB_SUPERVISOR = "LABS";
+
+}
