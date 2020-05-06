d96ee865fbef3ae46221a158dfb993203824680e
dmusial98 <dmusial98@gmail.com>
2020-05-06 20:04:49 +0200

    Changed format date

--- backend-application/src/main/resources/data.sql
+++ backend-application/src/main/resources/data.sql
@@ -265,7 +265,9 @@ insert into visit (id, receptionist_id, doctor_id, patient_id, state_id, descrip
 insert into visit (id, receptionist_id, doctor_id, patient_id, state_id, description, diagnose, registration_date, finalization_cancellation_date) values (19, 9, 19, 19, 1, 'Laceration with foreign body, left knee', 'S81022', '2020-05-16', '2020-05-28');
 insert into visit (id, receptionist_id, doctor_id, patient_id, state_id, description, diagnose, registration_date, finalization_cancellation_date) values (20, 10, 20, 20, 2, 'Crohn''s disease of both small and large intestine', 'K508', '2019-05-30', '2021-03-21');
 insert into visit (id, receptionist_id, doctor_id, patient_id, state_id, description, diagnose, registration_date) values (21, 10, 20, 20, 2, 'Crohn''s disease of both small and large intestine', 'K508', '2019-05-30');
-
+insert into visit (id, receptionist_id, doctor_id, patient_id, state_id, description, diagnose, registration_date, finalization_cancellation_date) values (22, 1, 3, 1, 1, 'Stomach pain', 'C123', '2017-05-06', '2020-10-04');
+insert into visit (id, receptionist_id, doctor_id, patient_id, state_id, description, diagnose, registration_date, finalization_cancellation_date) values (23, 1, 1, 1, 2, 'Leg pain', 'T123', '2019-09-02', '2018-04-02');
+insert into visit (id, receptionist_id, doctor_id, patient_id, state_id, description, diagnose, registration_date, finalization_cancellation_date) values (24, 1, 1, 2, 3, 'Fever', 'G123', '2017-12-12', '2019-07-03');
 
 insert into examination_category (code, name, type) values (1, 'Replacement of R Ext Carotid with Synth Sub, Open Approach', 1);
 insert into examination_category (code, name, type) values (2, 'Transfusion of Nonaut WBC into Central Vein, Perc Approach', 1);
