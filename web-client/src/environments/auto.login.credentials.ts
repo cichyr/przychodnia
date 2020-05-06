c2df04749073170236bdad0cde71c8feb4c7ec28
dmusial98 <dmusial98@gmail.com>
2020-05-06 19:19:06 +0200

    changed button for cancelation visit

--- web-client/src/environments/auto.login.credentials.ts
+++ web-client/src/environments/auto.login.credentials.ts
@@ -1,4 +1,4 @@
 export const dev_only_credentials = {
-  username: 'vwaldrum0',
-  password: '5e586bf6c9dfac3337a6e0b0c290fab9'
+  username: 'cberrycloth4',
+  password: '1d4fd0480f9642f2aa237c2b3de00d0d'
 };
