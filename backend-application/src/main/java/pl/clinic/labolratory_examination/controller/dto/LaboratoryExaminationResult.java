f737ece5564ab4063b5a95060386ef7554647688
podmati1998 <podmati@o2.pl>
2020-05-14 20:14:26 +0200

    naprawa json result

--- backend-application/src/main/java/pl/clinic/labolratory_examination/controller/dto/LaboratoryExaminationResult.java
+++ backend-application/src/main/java/pl/clinic/labolratory_examination/controller/dto/LaboratoryExaminationResult.java
@@ -1,12 +1,20 @@
 package pl.clinic.labolratory_examination.controller.dto;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class LaboratoryExaminationResult {
 
     protected String result;
-
-    public LaboratoryExaminationResult( String result) {
+    
+    public LaboratoryExaminationResult(String result) {
         this.result = result;
+    }
+
+    public LaboratoryExaminationResult()
+    {
+        super();
     }
 
     public String getResult() { return result; }
