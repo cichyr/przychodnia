a94a72da385466bfc2e0d1dc35438e21f48aa52b
Dominik Wolny <wolny.dominik@outlook.com>
2020-06-06 19:30:42 +0200

    Fixed fuckup - forgot to add new module in app.module, added NgbModule

--- web-client/src/app/admin-panel/admin-panel.module.ts
+++ web-client/src/app/admin-panel/admin-panel.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserListComponent } from './user-list/user-list.component';
 import { AddUserComponent } from './add-user/add-user.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 
 @NgModule({
   declarations: [UserListComponent, AddUserComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    NgbModule
   ]
 })
 export class AdminPanelModule { }
